67201edfc37bc1fb164749302fb8ab28c2695d5a
João Vitor <jvdrdo@gmail.com>
2024-09-28 15:31:20 -0300

    Update loginUsuarioDAO.java

--- src/DAO/loginUsuarioDAO.java
+++ src/DAO/loginUsuarioDAO.java
@@ -17,7 +17,7 @@ public class loginUsuarioDAO {
             //Inserindo usuario
             Connection coon = ConnectionMysql.openConnection();
 
-            String sqlUsuario = "INSERT INTO usuario (nome, ano_nascimento, cpf, email, ativo)+" +
+            String sqlUsuario = "INSERT INTO usuario (nome, ano_nascimento, cpf, email, ativo)" +
                     "VALUES (?,?,?,?,?)";
 
             PreparedStatement statementUsuario = coon.prepareStatement(sqlUsuario);
@@ -53,10 +53,42 @@ public class loginUsuarioDAO {
             statementLoginUsuario.executeUpdate();
 
             System.out.println("loginUsuario inserido");
-            
+
         }catch (SQLException e){
             System.out.println("Erro ao criar loginUsario: "+e.getMessage());
         }
         return null;
+    }
+
+    public loginUsuario aprovarLogin(String login, String senha){
+        loginUsuario aprovado = new loginUsuario();
+
+        try{
+            Connection coon = ConnectionMysql.openConnection();
+
+            String sqlBusca = "SELECT * FROM loginusuario join usuario on loginusuario.Usuario_idUsuario = usuario.idUsuario"+
+                    " WHERE loginusuario.login = ? and loginusuario.senha = ? LIMIT 1";
+
+            PreparedStatement statementBusca = coon.prepareStatement(sqlBusca);
+            statementBusca.setString(1, login);
+            statementBusca.setString(2, senha);
+
+            ResultSet resultSet = statementBusca.executeQuery();
+
+            while (resultSet.next()){
+                aprovado.setIdUsuario(resultSet.getInt("idUsuario"));
+                aprovado.setNome(resultSet.getString("nome"));
+                aprovado.setAno_nascimento(resultSet.getInt("ano_nascimento"));
+                aprovado.setCpf(resultSet.getString("cpf"));
+                aprovado.setEmail(resultSet.getString("email"));
+                aprovado.setLogin(login);
+                aprovado.setSenha(senha);
+            }
+
+        }catch (SQLException e){
+            System.out.println("Erro na busca: "+e.getMessage());
+        }
+
+        return aprovado;
     }
 }
